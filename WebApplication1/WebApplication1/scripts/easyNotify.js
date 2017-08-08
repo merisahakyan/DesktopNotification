952b7dfb03d0ddc67047a0550a25a647f77e8122
sahakyan.meri <sahakyan.meri@F4FS-01132>
2017-08-08 11:33:26 +0400

    test222

--- WebApplication1/WebApplication1/scripts/easyNotify.js
+++ WebApplication1/WebApplication1/scripts/easyNotify.js
@@ -16,7 +16,7 @@
 
         this.init = function () {
             //test 2
-            
+            //222
             var notify = this;
             if (!("Notification" in window)) {
                 alert("This browser does not support desktop notification");
